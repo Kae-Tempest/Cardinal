0c7663b15179fa6fc1a56dcf7ddcae216c0cfe06
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-06-21 00:27:49 +0200

    [WIP] Finish action battle loop

--- core/rpg/hunt.go
+++ core/rpg/hunt.go
@@ -11,10 +11,11 @@ import (
 	"math/rand"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 func CreateHuntFightThead(s *discordgo.Session, i *discordgo.InteractionCreate, playerName string, creatureName string) *discordgo.Channel {
-	// get Channel fight ID
 	channels, getChannelsErr := s.GuildChannels(i.GuildID)
 	if getChannelsErr != nil {
 
@@ -25,7 +26,6 @@ func CreateHuntFightThead(s *discordgo.Session, i *discordgo.InteractionCreate, 
 			parentID = channel.ID
 		}
 	}
-	// creation du fils privé
 	threadData := discordgo.ThreadStart{
 		Name:                fmt.Sprintf("%s VS %s", playerName, creatureName),
 		AutoArchiveDuration: 60,
@@ -58,59 +58,213 @@ func HuntFight(s *discordgo.Session, player entities.Player, creature entities.C
 	var playerChosenSkill entities.Skill
 	var creatureChosenSkill entities.Skill
 	var playerStats entities.Stats
+	var i int = 1
 
 	getPlayerStatErr := pgxscan.Get(ctx, db, &playerStats, `SELECT * FROM stats where user_id = $1`, player.ID)
 	if getPlayerStatErr != nil {
 		slog.Error("Error during getting Player stats in database", getPlayerStatErr)
 		return
 	}
+	for playerStats.HP > 0 || creature.HP > 0 {
+		if order[0].Name == "Player" {
+			playerSkill, creatureSkill = playerTurn(player, threadChannel, db, s), creatureTurn(creature, db)
+			if creatureSkill == nil || playerSkill == nil {
+				slog.Error("Error during choosing Skill")
+				return
+			}
+			getSkillErr := pgxscan.Get(ctx, db, &playerChosenSkill, `SELECT * from skills where id = $1`, playerSkill.ID)
+			getSkillErr = pgxscan.Get(ctx, db, &creatureChosenSkill, `SELECT * from skills where id = $1`, creatureSkill.ID)
 
-	if order[0].Name == "Player" {
-		playerSkill, creatureSkill = playerTurn(player, threadChannel, db, s), creatureTurn(creature, db)
-		if creatureSkill == nil || playerSkill == nil {
-			slog.Error("Error during choosing Skill")
-			return
-		}
-		getSkillErr := pgxscan.Get(ctx, db, &playerChosenSkill, `SELECT * from skills where name = $1`, playerSkill.ID)
-		getSkillErr = pgxscan.Get(ctx, db, &creatureChosenSkill, `SELECT * from skills where name = $1`, creatureSkill.ID)
+			if getSkillErr != nil {
+				slog.Error("Error during getting Skill in database", getSkillErr)
+			}
+		} else {
+			creatureSkill, playerSkill = creatureTurn(creature, db), playerTurn(player, threadChannel, db, s)
+			if creatureSkill == nil || playerSkill == nil {
+				slog.Error("Error during choosing Skill")
+				return
+			}
+			getSkillErr := pgxscan.Get(ctx, db, &playerChosenSkill, `SELECT * from skills where id = $1`, playerSkill.ID)
+			getSkillErr = pgxscan.Get(ctx, db, &creatureChosenSkill, `SELECT * from skills where id = $1`, creatureSkill.ID)
 
-		if getSkillErr != nil {
-			slog.Error("Error during getting Skill in database", getSkillErr)
+			if getSkillErr != nil {
+				slog.Error("Error during getting Skill in database", getSkillErr)
+			}
 		}
-	} else {
-		creatureSkill, playerSkill = creatureTurn(creature, db), playerTurn(player, threadChannel, db, s)
-		if creatureSkill == nil || playerSkill == nil {
-			slog.Error("Error during choosing Skill")
-			return
-		}
-		getSkillErr := pgxscan.Get(ctx, db, &playerChosenSkill, `SELECT * from skills where name = $1`, playerSkill.Name)
-		getSkillErr = pgxscan.Get(ctx, db, &creatureChosenSkill, `SELECT * from skills where name = $1`, creatureSkill.Name)
 
-		if getSkillErr != nil {
-			slog.Error("Error during getting Skill in database", getSkillErr)
-		}
-	}
+		if playerSkill.Type == creatureSkill.Type {
+			if playerSkill.Type == "attack" {
+				var damage int
+				switch playerChosenSkill.Type {
+				case "attack":
+					damage = playerChosenSkill.Strength + playerStats.Strength
+					creature.HP = creature.HP - damage
+					break
+				case "magic":
+					damage = playerChosenSkill.Intelligence + playerStats.Intelligence
+					creature.HP = creature.HP - damage
+					break
+				}
+				playerStats.Mana -= playerChosenSkill.Mana
 
-	if playerSkill.Type == creatureSkill.Type {
-		if playerSkill.Type == "attack" {
+				_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You did %d points of damage to %s", damage, creature.Name))
+				if sendErr != nil {
+					slog.Error("error during send message", sendErr)
+				}
+				switch creatureChosenSkill.Type {
+				case "attack":
+					damage = creatureChosenSkill.Intelligence + creature.Intelligence
+					playerStats.HP = playerStats.HP - damage
+					break
+				case "magic":
+					damage = creatureChosenSkill.Intelligence + creature.Intelligence
+					playerStats.HP = playerStats.HP - damage
+					break
+				}
+				creature.Mana -= creatureChosenSkill.Mana
+				// send msg for HP update
+				_, sendErr = s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("%s did %d points of damage to you", creature.Name, damage))
+				if sendErr != nil {
+					slog.Error("error during send message", sendErr)
+				}
+
+			} else {
+				_, err := s.ChannelMessageSend(threadChannel.ID, "Nothing append this turn...")
+				if err != nil {
+					slog.Error("Error during sending message", err)
+					return
+				}
+			}
+		} else if playerSkill.Type == "attack" && creatureSkill.Type == "block" {
+			var damage int
 			switch playerChosenSkill.Type {
 			case "attack":
-				creature.HP = creature.HP - (playerChosenSkill.Strength + playerStats.Strength)
+				damage = (playerChosenSkill.Strength + playerStats.Strength) - creature.Constitution
+				if damage < 0 {
+					damage = 0
+				}
+				creature.HP = creature.HP - damage
 				break
 			case "magic":
-				creature.HP = creature.HP - (playerChosenSkill.Intelligence + playerStats.Intelligence)
+				damage = (playerChosenSkill.Intelligence + playerStats.Intelligence) - creature.Constitution
+				if damage < 0 {
+					damage = 0
+				}
+				creature.HP = creature.HP - damage
 				break
 			}
 			playerStats.Mana -= playerChosenSkill.Mana
+			_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You did %d points of damage to %s", damage, creature.Name))
+			if sendErr != nil {
+				slog.Error("error during send message", sendErr)
+			}
+		} else if playerSkill.Type == "attack" && creatureSkill.Type == "dodge" {
+			if playerStats.Dexterity > creature.Dexterity {
+				var damage int
+				switch playerChosenSkill.Type {
+				case "attack":
+					damage = (playerChosenSkill.Strength + playerStats.Strength) * (5 / 100)
+					if damage < 0 {
+						damage = 0
+					}
+					creature.HP = creature.HP - damage
+					break
+				case "magic":
+					damage = (playerChosenSkill.Intelligence + playerStats.Intelligence) * (5 / 100)
+					if damage < 0 {
+						damage = 0
+					}
+					creature.HP = creature.HP - damage
+					break
+				}
+				_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You did %d points of damage to %s", damage, creature.Name))
+				if sendErr != nil {
+					slog.Error("error during send message", sendErr)
+				}
+			} else {
+				_, err := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("%s have dodge you're attack...", creature.Name))
+				if err != nil {
+					slog.Error("Error during sending message", err)
+					return
+				}
+			}
+			playerStats.Mana -= playerChosenSkill.Mana
+		} else if playerSkill.Type == "block" && creatureSkill.Type == "attack" {
+			var damage int
 			switch creatureChosenSkill.Type {
 			case "attack":
-				playerStats.HP = playerStats.HP - (creatureChosenSkill.Intelligence + creature.Intelligence)
+				damage = (creatureChosenSkill.Strength + creature.Strength) - playerStats.Constitution
+				if damage < 0 {
+					damage = 0
+				}
+				playerStats.HP = playerStats.HP - damage
 				break
 			case "magic":
-				playerStats.HP = playerStats.HP - (creatureChosenSkill.Intelligence + creature.Intelligence)
+				damage = (creatureChosenSkill.Intelligence + creature.Intelligence) - playerStats.Constitution
+				if damage < 0 {
+					damage = 0
+				}
+				playerStats.HP = playerStats.HP - damage
 				break
 			}
+			playerStats.Mana -= playerChosenSkill.Mana
+			// Send msg for HP update
+			_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You did %d points of damage to %s", damage, creature.Name))
+			if sendErr != nil {
+				slog.Error("error during send message", sendErr)
+			}
+		} else if playerSkill.Type == "dodge" && creatureSkill.Type == "attack" {
+			if playerStats.Dexterity < creature.Dexterity {
+				var damage int
+				switch creatureChosenSkill.Type {
+				case "attack":
+					damage = (creatureChosenSkill.Strength + creature.Strength) * (5 / 100)
+					playerStats.HP = playerStats.HP - damage
+					break
+				case "magic":
+					damage = (creatureChosenSkill.Intelligence + creature.Intelligence) * (5 / 100)
+					playerStats.HP = playerStats.HP - damage
+					break
+				}
+				_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("%s did %d points of damage to you", creature.Name, damage))
+				if sendErr != nil {
+					slog.Error("error during send message", sendErr)
+				}
+			} else {
+				_, err := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You have dodge %s attack...", creature.Name))
+				if err != nil {
+					slog.Error("Error during sending message", err)
+					return
+				}
+			}
 			creature.Mana -= creatureChosenSkill.Mana
+		} else if playerSkill.Type == "attack" && creatureSkill.Type == "dodge" {
+			var damage int
+			if playerStats.Dexterity > creature.Dexterity {
+				switch creatureChosenSkill.Type {
+				case "attack":
+					damage = (playerChosenSkill.Strength + playerStats.Strength) * (5 / 100)
+					if damage < 0 {
+						damage = 0
+					}
+					creature.HP = creature.HP - damage
+					break
+				case "magic":
+					damage = (playerChosenSkill.Intelligence + playerStats.Intelligence) * (5 / 100)
+					if damage < 0 {
+						damage = 0
+					}
+					creature.HP = creature.HP - damage
+					break
+				}
+			} else {
+				_, err := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("%s have dodge you're attack...", creature.Name))
+				if err != nil {
+					slog.Error("Error during sending message", err)
+					return
+				}
+			}
+			playerStats.Mana -= playerChosenSkill.Mana
 		} else {
 			_, err := s.ChannelMessageSend(threadChannel.ID, "Nothing append this turn...")
 			if err != nil {
@@ -118,77 +272,13 @@ func HuntFight(s *discordgo.Session, player entities.Player, creature entities.C
 				return
 			}
 		}
-	} else if playerSkill.Type == "attack" && creatureSkill.Type == "block" {
-		switch playerChosenSkill.Type {
-		case "attack":
-			creature.HP = creature.HP - ((playerChosenSkill.Strength + playerStats.Strength) - creature.Constitution)
-			break
-		case "magic":
-			creature.HP = creature.HP - ((playerChosenSkill.Intelligence + playerStats.Intelligence) - creature.Constitution)
-			break
-		}
-		playerStats.Mana -= playerChosenSkill.Mana
-	} else if playerSkill.Type == "attack" && creatureSkill.Type == "dodge" {
-		if playerStats.Dexterity > creature.Dexterity {
-			switch playerChosenSkill.Type {
-			case "attack":
-				creature.HP = creature.HP - ((playerChosenSkill.Strength + playerStats.Strength) * (5 / 100))
-				break
-			case "magic":
-				creature.HP = creature.HP - ((playerChosenSkill.Intelligence + playerStats.Intelligence) * (5 / 100))
-				break
-			}
-		} else {
-			_, err := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("%s have dodge you're attack...", creature.Name))
-			if err != nil {
-				slog.Error("Error during sending message", err)
-				return
-			}
-		}
-		playerStats.Mana -= playerChosenSkill.Mana
-	} else if playerSkill.Type == "block" && creatureSkill.Type == "attack" {
-		switch creatureChosenSkill.Type {
-		case "attack":
-			damage := (creatureChosenSkill.Strength + creature.Strength) - playerStats.Constitution
-			if damage < 0 {
-				damage = 0
-			}
-			playerStats.HP = playerStats.HP - damage
-			break
-		case "magic":
-			damage := (creatureChosenSkill.Intelligence + creature.Intelligence) - playerStats.Constitution
-			if damage < 0 {
-				damage = 0
-			}
-			playerStats.HP = playerStats.HP - damage
-			break
-		}
-		playerStats.Mana -= playerChosenSkill.Mana
-	} else if playerSkill.Type == "dodge" && creatureSkill.Type == "attack" {
-		if playerStats.Dexterity < creature.Dexterity {
-			switch creatureChosenSkill.Type {
-			case "attack":
-				playerStats.HP = playerStats.HP - ((creatureChosenSkill.Strength + creature.Strength) * (5 / 100))
-				break
-			case "magic":
-				playerStats.HP = playerStats.HP - ((creatureChosenSkill.Intelligence + creature.Intelligence) * (5 / 100))
-				break
-			}
-		} else {
-			_, err := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("You have dodge %s attack...", creature.Name))
-			if err != nil {
-				slog.Error("Error during sending message", err)
-				return
-			}
-		}
-		creature.Mana -= creatureChosenSkill.Mana
-	} else {
-		_, err := s.ChannelMessageSend(threadChannel.ID, "Nothing append this turn...")
-		if err != nil {
-			slog.Error("Error during sending message", err)
-			return
-		}
 	}
+	_, sendErr := s.ChannelMessageSend(threadChannel.ID, fmt.Sprintf("End of turn %d", i))
+	if sendErr != nil {
+		slog.Error("Error during sending message", sendErr)
+	}
+	i++
+	// while end
 }
 
 func creatureTurn(creature entities.Creatures, db *pgxpool.Pool) *skill {
@@ -215,11 +305,11 @@ func creatureTurn(creature entities.Creatures, db *pgxpool.Pool) *skill {
 		r := rand.Intn(10)
 		for _, skill := range basicCreatureSkill {
 			switch {
-			case r <= 5 && skill.Name == "attack":
+			case r <= 5 && skill.Type == "attack":
 				return skill
-			case r > 6 && r < 8 && skill.Name == "block":
+			case r > 6 && r < 8 && skill.Type == "block":
 				return skill
-			case r >= 8 && skill.Name == "dodge":
+			case r >= 8 && skill.Type == "dodge":
 				return skill
 			}
 		}
@@ -279,7 +369,7 @@ func playerTurn(player entities.Player, threadChannel *discordgo.Channel, db *pg
 
 	// Get 3 skill + atk de base + dodge + block
 	var playerSkills []entities.Skill
-	selectErr := pgxscan.Select(context.Background(), db, &playerSkills, `select id, name from skills s join user_skill us on s.id = us.skill_id join user_job_skill ujs on s.id = ujs.job_skill_id where us.user_id = $1 && ujs.user_id = $1`, player.ID)
+	selectErr := pgxscan.Select(context.Background(), db, &playerSkills, `select id, name from skills s join user_skill us on s.id = us.skill_id join user_job_skill ujs on s.id = ujs.job_skill_id where us.user_id = $1 and ujs.user_id = $1`, player.ID)
 	if selectErr != nil {
 		slog.Error("Error during selection player Skills into databases", selectErr)
 	}
@@ -355,26 +445,66 @@ func playerTurn(player entities.Player, threadChannel *discordgo.Channel, db *pg
 	}
 	// envoyer le messages des skills
 
-	_, err := s.ChannelMessageSendComplex(threadChannel.ID, &messageData)
+	message, err := s.ChannelMessageSendComplex(threadChannel.ID, &messageData)
 	if err != nil {
 		slog.Error("Error during sending the message", err)
 	}
-
-	//var ChoosenSkill *skill
 	// reception du choix du joueur
-	func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		data := i.ApplicationCommandData()
-		switch data.Name {
-		case "attack":
-			fmt.Println(data.Name)
-		case "block":
-			fmt.Println(data.Name)
-		case "dodge":
-			fmt.Println(data.Name)
-		case "skillselectbtn":
-			fmt.Println(data.Name)
-		}
-	}(s, i)
+	result, buttonInteractionErr := waitForButtonInteraction(s, message.ID, threadChannel.ID, 5*time.Minute)
+	if buttonInteractionErr != nil {
+		slog.Error("Error durring wait interraction", buttonInteractionErr)
+	}
 	// renvoie du skill choisie
+
+	switch result {
+	case "attack":
+		return basicPlayerSkill[0]
+	case "block":
+		return basicPlayerSkill[1]
+	case "dodge":
+		return basicPlayerSkill[2]
+
+	}
 	return nil
+}
+
+// waitForButtonInteraction waits for a button interaction and returns the CustomID of the clicked button.
+func waitForButtonInteraction(s *discordgo.Session, messageID, channelID string, timeout time.Duration) (string, error) {
+	interactionChannel := make(chan *discordgo.InteractionCreate)
+	defer close(interactionChannel)
+
+	// Temporary handler for this interaction
+	tempHandler := func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if interaction.Message.ID == messageID && interaction.ChannelID == channelID {
+			interactionChannel <- interaction
+		}
+	}
+
+	// Add the temporary handler
+	s.AddHandler(tempHandler)
+
+	// Create a wait group to handle timeout
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	var result *discordgo.InteractionCreate
+	var err error
+
+	go func() {
+		defer wg.Done()
+		select {
+		case result = <-interactionChannel:
+		case <-time.After(timeout):
+			err = fmt.Errorf("timeout waiting for interaction")
+		}
+	}()
+
+	// Wait for the interaction or timeout
+	wg.Wait()
+
+	if err != nil {
+		return "", err
+	}
+
+	return result.MessageComponentData().CustomID, nil
 }
