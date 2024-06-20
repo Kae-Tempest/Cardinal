5db3fb0aafafdc2a24e3d6aecc9eb019db6f3a08
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-06-20 13:00:11 +0200

    [WIP] Add Message Component Gestion

--- core/handler/interactioncreate.go
+++ core/handler/interactioncreate.go
@@ -13,17 +13,32 @@ func InteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	}
 	ctx := context.Background()
 	db := Cardinal.DatabaseConnect()
-	data := i.ApplicationCommandData()
-	switch data.Name {
-	case "ping":
-		commands.Ping(s, i)
-	case "setup":
-		commands.Setup(ctx, s, i, db)
-	case "move":
-		commands.Move(ctx, s, i, db)
-	case "harvest":
-		commands.Harvest(ctx, s, i, db)
-	case "hunt":
-		commands.Hunt(ctx, s, i, db)
+	switch i.Type {
+	case discordgo.InteractionMessageComponent:
+		data := i.MessageComponentData()
+		switch data.CustomID {
+		case "attack":
+			break
+		case "block":
+			break
+		case "dodge":
+			break
+		case "skillselectbtn":
+			break
+		}
+	case discordgo.InteractionApplicationCommand:
+		data := i.ApplicationCommandData()
+		switch data.Name {
+		case "ping":
+			commands.Ping(s, i)
+		case "setup":
+			commands.Setup(ctx, s, i, db)
+		case "move":
+			commands.Move(ctx, s, i, db)
+		case "harvest":
+			commands.Harvest(ctx, s, i, db)
+		case "hunt":
+			commands.Hunt(ctx, s, i, db)
+		}
 	}
 }
